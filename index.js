4f9f9ce7d8813697d025d77c616d1486e9acdccf
Kate Sandler <kate@katekatekate.net>
2019-05-24 17:53:27 -0700

    Sets up non-working but good-looking clock.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function digitalClock() {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let ampm = 'AM';
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = 'PM';
+  }
+
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+
+  let time = h + ':' + m + ':' + s + ' ' + ampm;
+  document.getElementById('clockDisplay').innerText = time;
+  document.getElementById('clockDisplay').textContent = time;
+}
+digitalClock();
